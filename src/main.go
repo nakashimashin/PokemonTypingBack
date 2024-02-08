4d75f079a0370acd769a3cdb5e487ab277d445b5
nakashimashin <nakashimashin0517@gmail.com>
2024-02-08 23:09:34 +0900

    main関数作成

--- src/main.go
+++ src/main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"back/controller"
+	"back/db"
+	"back/repository"
+	"back/router"
+	"back/usecase"
+)
+
 func main() {
+	db := db.NewDB()
+	userRepository := repository.NewUserRepository(db)
+	userUseCase := usecase.NewUserUseCase(userRepository)
+	userController := controller.NewUserController(userUseCase)
+	r := router.NewRouter(userController)
+	r.Run()
 }
